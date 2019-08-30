758593badbf8fa492200baf97943182b9ab7d521
Michael Johnson <michaelj@avianaglobal.com>
2019-08-30 16:59:10 -0700

    Use config file

--- revenewCC/ranking.py
+++ revenewCC/ranking.py
@@ -3,8 +3,8 @@ from gooey import Gooey, GooeyParser
 
 
 @Gooey(program_name='\nRevenewML\nCC Supplier Ranking\n',
-       dump_build_config=True,
-       # load_build_config='gooey_config.json',
+       # dump_build_config=True,
+       load_build_config='gooey_config.json',
        monospace_display=True,
        default_size=(760, 540))
 def main():
