addc725544973f79dd391a6086c1f46855885dad
Michael Johnson <michaelj@avianaglobal.com>
2019-10-08 16:26:38 -0700

    Success!!!

--- revenewCC/ranking.py
+++ revenewCC/ranking.py
@@ -218,12 +218,10 @@ def main():
     xref = pd.concat([matched, soft_matched], axis=0, sort=True, ignore_index=True).merge(commodity_df,
                                                                                           on='Supplier_ref')
 
-    len(xref.Supplier.unique())
-    len(xref.Supplier_ref.unique())
-
     final_df = input_df.merge(xref, on='Supplier').drop(columns='Cleaned')[
-        ['Client', 'Supplier', 'Supplier_ref', 'Commodity', 'Year', 'Total_Invoice_Amount', 'Total_Invoice_Count',
-         'Avg_Invoice_Size', ]]
+        ['Supplier', 'Supplier_ref', 'Commodity', 'Client', 'Year', 'Total_Invoice_Amount', 'Total_Invoice_Count',
+         'Avg_Invoice_Size']]
+    final_df
 
     # Scorecard computations
     logging.info('\nCalculating supplier scores based on scorecard...')
@@ -231,124 +229,92 @@ def main():
     # STEP 8b: do a full outer join with the scorecard
     final_df['key'] = 1
     supplier_scorecard['key'] = 1
-    scorecard_df = pd.merge(final_df, supplier_scorecard, on='key').drop('key', axis=1)
+    scorecard_df = pd.merge(final_df, supplier_scorecard, on='key').drop(columns='key')
 
     # STEP 8c-1: get the Spend sub-dataframe
-    scorecard_df_spend = (scorecard_df.loc[scorecard_df['Factor'] == 'Spend']).reset_index()
-
-    # find the matching record
+    scorecard_df_spend = scorecard_df.loc[scorecard_df['Factor'] == 'Spend'].reset_index()
     scorecard_df_spend = (
         scorecard_df_spend.loc[scorecard_df_spend['Total_Invoice_Amount'] < scorecard_df_spend['Max']])
-
     scorecard_df_spend = (
         scorecard_df_spend.loc[scorecard_df_spend['Total_Invoice_Amount'] > scorecard_df_spend['Min']])
-
     scorecard_df_spend = scorecard_df_spend.reset_index()
 
     # STEP 8c-2: #get the InvoiceSize sub-dataframe
-    scorecard_df_invoicesize = (scorecard_df.loc[scorecard_df['Factor'] == 'InvoiceSize']).reset_index()
-
-    # find the matching record
+    scorecard_df_invoicesize = scorecard_df.loc[scorecard_df['Factor'] == 'InvoiceSize'].reset_index()
     scorecard_df_invoicesize = (
         scorecard_df_invoicesize.loc[scorecard_df_invoicesize['Avg_Invoice_Size'] < scorecard_df_invoicesize['Max']])
-
     scorecard_df_invoicesize = (
         scorecard_df_invoicesize.loc[scorecard_df_invoicesize['Avg_Invoice_Size'] > scorecard_df_invoicesize['Min']])
-
     scorecard_df_invoicesize = scorecard_df_invoicesize.reset_index()
 
     # STEP 8c-3: #get the InvoiceCount sub-dataframe
-    scorecard_df_invoicecount = (scorecard_df.loc[scorecard_df['Factor'] == 'InvoiceCount']).reset_index()
-
-    # find the matching record
-    scorecard_df_invoicecount = (scorecard_df_invoicecount.loc[
-        scorecard_df_invoicecount['Total_Invoice_Count'] < scorecard_df_invoicecount['Max']])
-
-    scorecard_df_invoicecount = (scorecard_df_invoicecount.loc[
-        scorecard_df_invoicecount['Total_Invoice_Count'] > scorecard_df_invoicecount['Min']])
-
+    scorecard_df_invoicecount = scorecard_df.loc[scorecard_df['Factor'] == 'InvoiceCount'].reset_index()
+    scorecard_df_invoicecount = scorecard_df_invoicecount.loc[
+        scorecard_df_invoicecount['Total_Invoice_Count'] < scorecard_df_invoicecount['Max']]
+    scorecard_df_invoicecount = scorecard_df_invoicecount.loc[
+        scorecard_df_invoicecount['Total_Invoice_Count'] > scorecard_df_invoicecount['Min']]
     scorecard_df_invoicecount = scorecard_df_invoicecount.reset_index()
 
     # STEP 8c-4: get the Commodity sub-dataframe
-    scorecard_df_commodity = (scorecard_df.loc[scorecard_df['Factor'] == 'Commodity']).reset_index()
-
-    # find the matching record
+    scorecard_df_commodity = scorecard_df.loc[scorecard_df['Factor'] == 'Commodity'].reset_index()
     scorecard_df_commodity = (
         scorecard_df_commodity.loc[scorecard_df_commodity['Commodity'] == scorecard_df_commodity['Tier']])
-
     scorecard_df_commodity = scorecard_df_commodity.reset_index()
 
     # STEP 8d: # append all the factor scores
-    scores = scorecard_df_spend.append(scorecard_df_invoicesize, ignore_index=True)
-    scores = scores.append(scorecard_df_invoicecount, ignore_index=True)
-    scores = scores.append(scorecard_df_commodity, ignore_index=True)
+    scores = pd.concat(
+        [scorecard_df_spend, scorecard_df_invoicesize, scorecard_df_invoicecount, scorecard_df_commodity], axis=0,
+        sort=False).drop(columns=['level_0', 'index'])
 
     # score at supplier-year-factor-tier level
-    component_scores = ...
+    component_scores = scores.groupby(['Supplier_ref', 'Year', 'Factor']).sum().stack().unstack()
 
     # score at supplier-year level
-    scores = ...
+    component_scores = scores.groupby(['Supplier_ref', 'Year']).sum().stack().unstack().unstack(level=1)
     logging.info(f'\nWriting output file to {outputdir}...')
+    #
+    # # STEP 9:  format in the way scorecard is currently implemented
+    # num_years = len(component_scores['Year'].unique())
+    # years = component_scores['Year'].drop_duplicates()
+    # years = np.sort(years)
+    # Yr_data_List = list()
+    # i = 0
+    # while i < num_years:
+    #     Yr = years[i]
+    #     Yr_data = component_scores.loc[component_scores['Year'] == Yr]
+    #     Yr_data = Yr_data.groupby(['Client', 'Supplier_ref', 'Year'])[
+    #         'Total_Invoice_Amount', 'Total_Invoice_Count', 'Avg_Invoice_Size'].max()
+    #     Yr_data = (Yr_data.rename(columns={'Total_Invoice_Amount_Max': 'Total_Invoice_Amount',
+    #                                        'Total_Invoice_Count_Max': 'Total_Invoice_Count',
+    #                                        'Avg_Invoice_Size_Max': 'Avg_Invoice_Size'}))
+    #     Yr_data_comm = (
+    #         component_scores.loc[(component_scores['Year'] == Yr) & (component_scores['Factor'] == "Commodity")][
+    #             ['Client', 'Supplier_ref', 'Year', 'Original_Commodity']])
+    #     Yr_data = pd.merge(Yr_data, Yr_data_comm, on=['Client', 'Supplier_ref', 'Year'])
+    #     Yr_score_data = scores.loc[scores['Year'] == Yr]
+    #     Yr_data = pd.merge(Yr_data, Yr_score_data, on=['Client', 'Supplier_ref', 'Year'])
+    #     Yr_data = Yr_data[
+    #         ['Client', 'Supplier_ref', 'Original_Commodity', 'Total_Invoice_Amount', 'Total_Invoice_Count',
+    #          'Avg_Invoice_Size', 'Points_Sum']]
+    #     Yr_data = Yr_data.rename(
+    #         columns={'Client': 'Client', 'Total_Invoice_Amount': str(int(Yr)) + "_Total_Invoice_Amount",
+    #                  'Total_Invoice_Count': str(int(Yr)) + "_Total_Invoice_Count",
+    #                  'Avg_Invoice_Size': str(int(Yr)) + "_Avg_Invoice_Size", 'Points_Sum': str(int(Yr)) + "_Score"})
+    #     Yr_data_List.append(Yr_data)
+    #     i = i + 1
+    # final_data = Yr_data_List[0]
+    # i = 1
+    # while i < len(Yr_data_List):
+    #     yr_df = Yr_data_List[i]
+    #     yr_df = yr_df.drop(['Original_Commodity'], axis=1)
+    #     final_data = (pd.merge(yr_df, final_data, on=['Client', 'Supplier_ref']))
+    #     i = i + 1
+    # cols = list(final_data.columns.values)  # Make a list of all of the columns in the df
+    # cols.pop(cols.index('Client'))
+    # cols.pop(cols.index('Supplier_ref'))
+    # cols.pop(cols.index('Original_Commodity'))
+    # final_data = final_data[['Client', 'Supplier_ref', 'Original_Commodity'] + cols]
 
-    ####################################
-    # STEP 9:  format in the way scorecard is currently implemented
-    num_years = len(component_scores['Year'].unique())
-    years = component_scores['Year'].unique()
-    years = np.sort(years)
-
-    Yr_data_List = list()
-
-    i = 0
-    while i < num_years:
-        Yr = years[i]
-        Yr_data = component_scores.loc[component_scores['Year'] == Yr]
-
-        Yr_data = (helpers.group_by_stats_list_max(Yr_data, ['Client', 'Supplier_ref', 'Year'],
-                                                   ['Total_Invoice_Amount', 'Total_Invoice_Count', 'Avg_Invoice_Size'])[
-            ['Client', 'Supplier_ref', 'Year', 'Total_Invoice_Amount_Max', 'Total_Invoice_Count_Max',
-             'Avg_Invoice_Size_Max']])
-
-        Yr_data = (Yr_data.rename(columns={'Total_Invoice_Amount_Max': 'Total_Invoice_Amount',
-                                           'Total_Invoice_Count_Max': 'Total_Invoice_Count',
-                                           'Avg_Invoice_Size_Max': 'Avg_Invoice_Size'}))
-
-        Yr_data_comm = (
-            component_scores.loc[(component_scores['Year'] == Yr) & (component_scores['Factor'] == "Commodity")][
-                ['Client', 'Supplier_ref', 'Year', 'Original_Commodity']])
-
-        Yr_data = pd.merge(Yr_data, Yr_data_comm, on=['Client', 'Supplier_ref', 'Year'])
-
-        Yr_score_data = scores.loc[scores['Year'] == Yr]
-        Yr_data = pd.merge(Yr_data, Yr_score_data, on=['Client', 'Supplier_ref', 'Year'])
-        Yr_data = Yr_data[
-            ['Client', 'Supplier_ref', 'Original_Commodity', 'Total_Invoice_Amount', 'Total_Invoice_Count',
-             'Avg_Invoice_Size', 'Points_Sum']]
-        Yr_data = Yr_data.rename(
-            columns={'Client': 'Client', 'Total_Invoice_Amount': str(int(Yr)) + "_Total_Invoice_Amount",
-                     'Total_Invoice_Count': str(int(Yr)) + "_Total_Invoice_Count",
-                     'Avg_Invoice_Size': str(int(Yr)) + "_Avg_Invoice_Size", 'Points_Sum': str(int(Yr)) + "_Score"})
-
-        Yr_data_List.append(Yr_data)
-        i = i + 1
-
-    final_data = Yr_data_List[0]
-    i = 1
-    while i < len(Yr_data_List):
-        yr_df = Yr_data_List[i]
-        yr_df = yr_df.drop(['Original_Commodity'], axis=1)
-
-        final_data = (pd.merge(yr_df, final_data, on=['Client', 'Supplier_ref']))
-        i = i + 1
-
-    # Create new dataframe with columns in the order you want
-    cols = list(final_data.columns.values)  # Make a list of all of the columns in the df
-    cols.pop(cols.index('Client'))
-    cols.pop(cols.index('Supplier_ref'))
-    cols.pop(cols.index('Original_Commodity'))
-    final_data = final_data[['Client', 'Supplier_ref', 'Original_Commodity'] + cols]
-
-    ####################################
-    # STEP 10:  Write out all results into a spreadsheet
     # Create a Pandas Excel writer using XlsxWriter as the engine.
     writer = pd.ExcelWriter(f'{outputdir}/CC_Audit_Scorecard.xlsx', engine='xlsxwriter')
 
@@ -356,10 +322,9 @@ def main():
     matched.to_excel(writer, sheet_name='CrossRef_Matched_Suppliers', index=False)
     unmatched.to_excel(writer, sheet_name='CrossRef_unMatched_Suppliers', index=False)
     best_matches.to_excel(writer, sheet_name='SoftMatched_Suppliers', index=False)
-    unmatched.to_excel(writer, sheet_name='NoSoft_Matched_Supp', index=False)
-    component_scores.to_excel(writer, sheet_name='Component_Scores', index=False)
+    component_scores.to_excel(writer, sheet_name='Component_Scores')
     scores.to_excel(writer, sheet_name='SupplierScoreCard', index=False)
-    final_data.to_excel(writer, sheet_name='FinalScorecard', index=False)
+    # final_data.to_excel(writer, sheet_name='FinalScorecard', index=False)
     writer.save()
 
     # Stop timer
