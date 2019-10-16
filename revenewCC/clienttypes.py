7f6493f90b15ed7572ae351324f0e12c36c68f16
Michael Johnson <michaelj@avianaglobal.com>
2019-10-16 12:34:35 -0700

    Refactor client types

--- revenewCC/clienttypes.py
+++ revenewCC/clienttypes.py
@@ -1,8 +1,8 @@
-from tqdm.auto import tqdm
 import pandas as pd
-
+import numpy as np
 
 def input_spr(database):
+    
     dataquery = f"""
     SELECT Supplier,
            datename(YEAR, Invoice_Date) AS Year,
@@ -40,7 +40,7 @@ def input_spr(database):
 
 
 def input_nonspr_rollup(filename, clientname, logging):
-    input_df = pd.read_csv(filename, encoding='ISO-8859-1', chunksize=1)
+    input_df = pd.read_csv(filename, encoding='ISO-8859-1', low_memory=False)
     expected_columns = ['Supplier', 'Total_Invoice_Amount', 'Total_Invoice_Count', 'Year']
     inlist = [col in input_df.columns for col in expected_columns]
     if sum(inlist) != len(expected_columns):
--- revenewCC/dbupdate.py
+++ revenewCC/dbupdate.py
@@ -1,9 +1,35 @@
 import pandas as pd
 from tqdm import tqdm
-import revenewCC.dbconnect
+import sys
+
+def dbconnect():
+    # Monkey patch for macOS
+    if sys.platform == 'darwin':
+        host = '208.43.250.18'
+        port = '51949'
+        user = 'sa'
+        password = 'Aviana$92821'
+        database = 'AvianaML'
+        driver = '/usr/local/lib/libmsodbcsql.13.dylib'
+        cnxn_str = f'mssql+pyodbc://{user}:{password}@{host}:{port}/{database}?driver={driver}'
+    else:
+        dsn = 'cc'
+        cnxn_str = f'mssql+pyodbc://@{dsn}'
+
+    # Make database connection engine
+    from sqlalchemy import create_engine
+
+    # Options below are useful for debugging
+    engine = create_engine(cnxn_str)
+    return engine.connect()
+
+
+def sqliteconnect():
+    import sqlite3
+    return sqlite3.connect('revenewCC/inputdata/revenewCC.db')
 
 # Set up data connection
-engine = revenewCC.dbconnect.dbconnect()
+engine = dbconnect()
 
 
 def xref_update(supplier_crossref_list):
